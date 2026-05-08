ef57bcdbe7f56587ed467a380503e065ac171075
Swayam User <user@example.com>
2026-05-08 13:05:07 +0530

    refactor: remove redundant swayam_github directory and clean up repository root

--- swayam_github/swayam/conftest.py
+++
@@ -1,6 +0,0 @@
-                                            
-import sys
-import os
-
-# Make sure src/ is always on the path when running pytest from project root
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), "src"))
